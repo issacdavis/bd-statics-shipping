1249f266e111770549601f2347a3109e3fc6d1f2
issacdavis <issac.davis.1998@gmail.com>
2024-09-14 13:18:01 -0400

    tests passed

--- src/com/amazon/ata/statics/TrackedBox.java
+++ src/com/amazon/ata/statics/TrackedBox.java
@@ -1,21 +1,28 @@
 package com.amazon.ata.statics;
+import java.util.UUID;
 
 public class TrackedBox {
+    private static String boxId;
+    private static String trackingId;
 
     /**
      * Constructs a TrackedBox object. A trackingId will also be generated for this TrackedBox object.
      * @param boxId - the id of the box
      */
     public TrackedBox(String boxId) {
-
+        TrackedBox.boxId = boxId;
+        TrackedBox.trackingId = createTrackingId();
     }
 
     public String getTrackingId() {
-        return null;
+        return createTrackingId();
     }
 
     public String getBoxId() {
-        return null;
+        return TrackedBox.boxId;
+    }
+    private String createTrackingId() {
+        return TrackedBox.boxId + UUID.randomUUID();
     }
 
 }
